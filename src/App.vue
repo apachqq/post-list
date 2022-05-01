33fab80472433a9651fe12ddd5f66e9ff0ce7d5a
apachqq <work.tsyganov@mail.ru>
2022-05-01 11:31:26 +0300

    Bilateral-binding

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-    <div>
+    <div class="app">
         <div>
             <form>
-                <input type="text" placeholder="Название">
-                <input type="text" placeholder="Описание">
+                <h4>Создание поста</h4>
+                <input
+                        :value="title"
+                        @input="title = $event.target.value"
+                        class="input"
+                        type="text"
+                        placeholder="Название"
+                >
+                <input
+                        :value="body"
+                        @input="body = $event.target.value"
+                        class="input"
+                        type="text"
+                        placeholder="Описание"
+                >
+                <button class="btn" @click="createPost">Создать</button>
             </form>
         </div>
         <div class="post" v-for="post in posts">
@@ -21,7 +35,14 @@
                     {id: 1, title: 'JavaScript', body: 'Описание поста'},
                     {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
                     {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'}
-                ]
+                ],
+                title: '',
+                body: ''
+            }
+        },
+        methods: {
+            createPost() {
+
             }
         }
     }
@@ -34,9 +55,34 @@
         box-sizing: border-box;
     }
 
+    .app {
+        padding: 20px;
+    }
+
     .post {
         padding: 15px;
         border: 2px solid teal;
         margin-top: 15px;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .input {
+        width: 100%;
+        border: 1px solid teal;
+        padding: 10px 15px;
+        margin-top: 15px;
+    }
+
+    .btn {
+        margin-top: 15px;
+        align-self: flex-end;
+        padding: 10px 15px;
+        background: none;
+        color: teal;
+        border: 1px solid teal;
     }
 </style>
