d66e1fa12cdedac0e186cdc5c6646e07a260c6df
apachqq <work.tsyganov@mail.ru>
2022-05-04 09:53:32 +0300

    animation-add-post-and-delete-post-with-transition-group

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
         <h3>Список постов</h3>
-        <post-item
-                v-for="post in posts"
-                :post="post"
-                :key="post.id"
-                @remove="$emit('remove', post)"
-        ></post-item>
+        <transition-group name="post-list">
+            <post-item
+                    v-for="post in posts"
+                    :post="post"
+                    :key="post.id"
+                    @remove="$emit('remove', post)"
+            ></post-item>
+        </transition-group>
     </div>
 </template>
 
@@ -25,4 +27,17 @@
 </script>
 
 <style scoped>
+    .post-list-item {
+        display: inline-block;
+        margin-right: 10px;
+    }
+    .post-list-enter-active,
+    .post-list-leave-active {
+        transition: all 0.4s ease;
+    }
+    .post-list-enter-from,
+    .post-list-leave-to {
+        opacity: 0;
+        transform: translateX(130px);
+    }
 </style>
