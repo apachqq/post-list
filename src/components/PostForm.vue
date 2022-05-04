66b839def2bbe9f2ff2cc67187e2d71195807e0c
apachqq <work.tsyganov@mail.ru>
2022-05-04 09:44:43 +0300

    if-else

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,21 +1,22 @@
 <template>
-        <form @submit.prevent>
-            <h4>Создание поста</h4>
-            <my-input
-                    v-model="post.title"
-                    type="text"
-                    placeholder="Название"
-            ></my-input>
-            <my-input
-                    v-model="post.body"
-                    type="text"
-                    placeholder="Описание"
-            ></my-input>
-            <my-button
-                    style="align-self: flex-end; margin-top: 15px;"
-                    @click="createPost"
-            >Создать</my-button>
-        </form>
+    <form @submit.prevent>
+        <h4>Создание поста</h4>
+        <my-input
+                v-model="post.title"
+                type="text"
+                placeholder="Название"
+        ></my-input>
+        <my-input
+                v-model="post.body"
+                type="text"
+                placeholder="Описание"
+        ></my-input>
+        <my-button
+                style="align-self: flex-end; margin-top: 15px;"
+                @click="createPost"
+        >Создать
+        </my-button>
+    </form>
 </template>
 
 <script>
@@ -30,11 +31,15 @@
         },
         methods: {
             createPost() {
-                this.post.id = Date.now()
-                this.$emit('create', this.post)
-                this.post = {
-                    title: '',
-                    body: ''
+                if (this.post.title !== '' && this.post.body !== '') {
+                    this.post.id = Date.now()
+                    this.$emit('create', this.post)
+                    this.post = {
+                        title: '',
+                        body: ''
+                    }
+                } else {
+                    alert('Введите название и описание поста')
                 }
             }
         }
