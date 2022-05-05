a4c9708260b12aa6730841859f821e8bd90518bb
apachqq <work.tsyganov@mail.ru>
2022-05-05 12:35:24 +0300

    pagination

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,15 @@
         <div v-else>
             <the-loader></the-loader>
         </div>
+        <div class="page_wrapper">
+            <div
+                    v-for="page in totalPages"
+                    :key="page"
+                    class="page"
+            >
+                {{ page }}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -45,6 +54,9 @@
                 isPostsLoading: false,
                 selectedSort: '',
                 searchQuery: '',
+                page: 1,
+                limit: 10,
+                totalPages: 0,
                 sortOptions: [
                     {value: 'title', name: 'По названию'},
                     {value: 'body', name: 'По описанию'}
@@ -65,7 +77,13 @@
             async fetchPosts() {
                 try {
                     this.isPostsLoading = true
-                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                        params: {
+                            _page: this.page,
+                            _limit: this.limit
+                        }
+                    })
+                    this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
                     this.posts = response.data
                 } catch (e) {
                     alert('Ошибка')
@@ -115,5 +133,19 @@
         display: flex;
         justify-content: space-between;
         margin: 15px 0;
+    }
+
+    .page_wrapper {
+        display: flex;
+        margin-top: 15px;
+    }
+
+    .page {
+        border: 1px solid black;
+        padding: 10px;
+    }
+
+    .current-page {
+        border: 2px solid teal;
     }
 </style>
