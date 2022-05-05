3b6816b0aa39e843f352d1a1b0793cabeb46777f
apachqq <work.tsyganov@mail.ru>
2022-05-05 14:03:45 +0300

    vue-router

---
+++ src/pages/AboutUs.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>Данное приложение было создано для закрепления навыков, и получения новых</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from '@/pages/MainPage'
+import PostPage from '@/pages/PostPage'
+import AboutUs from '@/pages/AboutUs'
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes : []
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: MainPage
+        },
+        {
+            path: '/posts',
+            component: PostPage
+        },
+        {
+            path: '/about',
+            component: AboutUs
+        }
+    ]
 })
 
 export default router
