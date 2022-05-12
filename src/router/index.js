a07df9958f6051fd2d9fd6ef4f720efa1ded3bea
apachqq <work.tsyganov@mail.ru>
2022-05-12 13:50:06 +0300

    404-error

--- src/router/index.js
+++ src/router/index.js
@@ -3,26 +3,16 @@ import MainPage from '@/pages/MainPage'
 import PostPage from '@/pages/PostPage'
 import AboutUs from '@/pages/AboutUs'
 import PostIdPage from '@/pages/PostIdPage'
+import PageNotFound from '@/pages/PageNotFound'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {
-            path: '/',
-            component: MainPage
-        },
-        {
-            path: '/posts',
-            component: PostPage
-        },
-        {
-            path: '/about',
-            component: AboutUs
-        },
-        {
-            path: '/posts/:id',
-            component: PostIdPage
-        }
+        {path: '/', component: MainPage},
+        {path: '/posts', component: PostPage},
+        {path: '/about', component: AboutUs},
+        {path: '/posts/:id', component: PostIdPage},
+        { path: '/:notFound(.*)', name: 'PageNotFound', component: PageNotFound },
     ]
 })
 
